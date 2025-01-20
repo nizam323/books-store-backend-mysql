82a61d54dab9e33bf7d377b03c24945c4acd3e41
nizam323 <muhammadnizamuddin.sce@gmail.com>
2025-01-20 21:03:45 +0500

    vercel.json added

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const db = mysql.createConnection({
   host: process.env.host,
   user: process.env.user,
   password: process.env.password,
-  database: process.env.database
+  database: process.env.database,
+  port:process.env.databasePort
 });
 
 db.connect((err) => {
